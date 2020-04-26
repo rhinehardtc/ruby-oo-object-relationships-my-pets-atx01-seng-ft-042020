5d4a4beda68e74af9a4c5f7497a9ec6dc42de76c
rhinehardtc <rhinehardtc@gmail.com>
2020-04-25 23:58:35 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,17 @@
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @@all << self
+    @mood = "nervous"
+  end
+
+  def self.all
+    @@all
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,64 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    all_pets = self.cats + self.dogs
+    all_pets.each do |pet| 
+      pet.mood = "nervous"
+      pet.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all = []
+  end
 end
